a3cd23327a10f9e1b4880496986e14b1ed361774
ameelshareif01-beep <ameelshareif01@gmail.com>
2025-09-19 19:39:10 -0700

    Create DynamicArray.java

---
+++ DynamicArray.java
@@ -0,0 +1,58 @@
+public class DynamicArray<T> {
+    private T[] data;
+    private int size;
+    private static final int INITIAL_CAPACITY = 10;
+
+    @SuppressWarnings("unchecked")
+    public DynamicArray() {
+        data = (T[]) new Object[INITIAL_CAPACITY];
+        size = 0;
+    }
+
+    public void add(T element) {
+        if (size == data.length) {
+            resize();
+        }
+        data[size++] = element;
+    }
+
+    public T get(int index) {
+        checkIndex(index);
+        return data[index];
+    }
+
+    public T remove(int index) {
+        checkIndex(index);
+        T removedElement = data[index];
+
+        // Shift elements left
+        for (int i = index; i < size - 1; i++) {
+            data[i] = data[i + 1];
+        }
+        data[size - 1] = null; // Prevent memory leak
+        size--;
+
+        return removedElement;
+    }
+
+    public int size() {
+        return size;
+    }
+
+    @SuppressWarnings("unchecked")
+    private void resize() {
+        T[] newData = (T[]) new Object[data.length * 2];
+        for (int i = 0; i < data.length; i++) {
+            newData[i] = data[i];
+        }
+        data = newData;
+    }
+
+    private void checkIndex(int index) {
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException(
+                "Index: " + index + ", Size: " + size
+            );
+        }
+    }
+}
